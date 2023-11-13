0e6ffa54ab4edc87924d1b49a742c1ffba792ca5
Matt Mazzola <mattm@microsoft.com>
2023-11-13 12:34:36 -0800

    Add branch to LLaVA fork clone (#9)

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 echo "Cloning dependent repos..."
 git clone --single-branch https://github.com/mattmazzola/GLIGEN.git
 git clone --single-branch https://github.com/mattmazzola/Segment-Everything-Everywhere-All-At-Once.git SEEM
-git clone --single-branch https://github.com/mattmazzola/LLaVA
+git clone --single-branch -b make_usable_from_other_demos https://github.com/mattmazzola/LLaVA
 git clone --single-branch https://github.com/advimman/lama.git
 
 echo "Creating environments and download pretrained models..."
