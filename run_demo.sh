0965827e751c42073b0c74f839d2074765838a21
Matt Mazzola <mattm@microsoft.com>
2024-02-07 21:28:24 +0000

    Remove all moderation flags

--- run_demo.sh
+++ run_demo.sh
@@ -58,21 +58,10 @@ if [ "$RUN_LLAVA_INT" = "True" ]; then
     conda deactivate
     conda activate llava_int
 
-    export LOGLEVEL=DEBUG
     export LLAVA_INTERACTIVE_HOME=.
     export GRADIO_NO_RELOAD=True
 
-    python llava_interactive.py \
-      --moderate \
-      input_text_aics \
-      input_text_guardlist \
-      input_text_aics_jailbreak \
-      input_image_aics \
-      output_text_guardlist \
-      output_text_aics \
-      gligen_input_text_guardlist \
-      gligen_input_text_aics \
-      gligen_output_image_aics
+    python llava_interactive.py
   )
 else
   echo "Skipping llava_interactive.py because --debug was given."
